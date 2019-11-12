934ae4cd53f534f318ed6639f2984ffd3a4ee148
HyuriMaciel <smhyuri@gmail.com>
2019-11-12 08:45:46 -0300

    sr2

--- codigos/ajudeAmbrosio.py
+++ codigos/ajudeAmbrosio.py
@@ -1,18 +1,18 @@
 
-
 class Node(object):
     def __init__(self, start, end):
         self.start = start
         self.end = end
+        self.par =0
+        self.imp =0
         self.sum = 0
-        self.par = 0
-        self.impar = 0
         self.left = None
         self.right = None
 
 
 class segmentTree(object):
     def __init__(self, vector):
+       # print("----",nums)
 
         def createTree(vector, left, rigth):
             #print(l,r)
@@ -21,12 +21,11 @@ class segmentTree(object):
 
             if left == rigth:
                 n = Node(left, rigth)
-                if vector[left] % 2 == 0:
-
+                if vector[left]% 2 == 0:
                     n.par = 1
                 else:
-
-                    n.impar = 1
+                    n.imp = 1
+                n.sum = vector[left]
                 return n
 
             mid = (left + rigth) // 2
@@ -35,75 +34,101 @@ class segmentTree(object):
             st.left = createTree(vector, left, mid)
             st.right = createTree(vector, mid +1, rigth)
 
+            st.sum = st.left.sum + st.right.sum
             st.par = st.left.par + st.right.par
-            st.impar = st.left.impar + st.right.impar
-
+            st.imp = st.left.imp + st.right.imp
             return st
 
-        self.st = createTree(vector, 0, len(vector)-1)
+        self.st = createTree(vector, 0, len(vector) -1)
 
     def update(self, current, val):
 
         def updateVal(st, current, val):
-
+            p = 1
+            i = 1
             if st.start == st.end:
+                #print(st.start,st.end)
                 if val % 2 == 0:
-                    st.par = 1
+                    print(val)
+                    st.par += p
+                    st.sum = val
                 else:
-                    st.impar = 0
+                    st.imp += i
+                    st.sum = val
 
-                return val
+                return st.par,st.imp,st.sum
             mid = (st.start + st.end) // 2
 
             if current <= mid:
                 updateVal(st.left,current, val)
             else:
                 updateVal(st.right, current, val)
+            st.sum = st.left.sum + st.right.sum
             st.par = st.left.par + st.right.par
-            st.impar = st.left.impar + st.right.impar
-            return st.par, st.impar
+            st.imp = st.left.imp + st.right.imp
+            return st.par
 
         return updateVal(self.st, current, val)
 
-    def sumRange(self, i, j,pi):
+    def sumRangep(self, i, j):
 
-        def rangeSum(st, i, j,pi):
+        def rangeSump(st, i, j):
 
             if st.start == i and st.end == j:
-                if pi == 1 :
-                    return st.par
-                elif pi == 2:
-                    return st.impar
+                #print(st.par,st.imp)
+                return st.par
 
             mid = (st.start + st.end) // 2
 
             if j <= mid:
-                return rangeSum(st.left, i, j)
+                return rangeSump(st.left, i, j)
 
             elif i >= mid + 1:
-                return rangeSum(st.right, i, j,pi)
+                return rangeSump(st.right, i, j)
 
             else:
-                return rangeSum(st.left, i, mid,pi) + rangeSum(st.right, mid +1, j,pi)
+                return rangeSump(st.left, i, mid) + rangeSump(st.right, mid +1, j)
 
-        return rangeSum(self.st, i, j,pi)
+        return rangeSump(self.st, i, j)
+
+    def sumRangei(self, i, j):
+
+        def rangeSumi(st, i, j):
+
+            if st.start == i and st.end == j:
+                #print(st.par,st.imp)
+                return st.imp
+
+            mid = (st.start + st.end) // 2
+
+            if j <= mid:
+                return rangeSumi(st.left, i, j)
+
+            elif i >= mid + 1:
+                return rangeSumi(st.right, i, j)
+
+            else:
+                return rangeSumi(st.left, i, mid) + rangeSumi(st.right, mid +1, j)
+
+        return rangeSumi(self.st, i, j)
 
 
 
 
-# n = int(input())
-# A = input()
-# A = A.split(' ')
-#
-# for i, e in enumerate(A):
-#     A[i] = int(e)
-# #print(A)
-# q = int(input())
-#
-# R = []
-#
-# for i in range(q):
-#     R.append((input()).split())
+#n = int(input())
+#A = input()
+#A = A.split(' ')
+
+#for i, e in enumerate(A):
+#    A[i] = int(e)
+#print(A)
+#q = int(input())
+
+#R = []
+
+#for i in range(q):
+#    R.append((input()).split())
+#print(R)
 
 
 n = 6
@@ -111,29 +136,24 @@ A = [1,2,3,4,5,6]
 q = 4
 R = [[1,2,5],[2,1,4],[0,5,4],[1,1,6]]
 
-
-
 tree = segmentTree(A)
 
 
-#print(tree.sumRange(int(R[0][1]),int(R[0][2]),2))
-# print(tree2.sumRange2(int(R[0][1]),int(R[0][2])))
-
 for i in range(q):
-    if(R[i][0] == 1):
-        if R[i][2] == A[len(A)-1]:
-            print(tree.sumRange(R[i][1],R[i][2]-1,1))
+    if(int(R[i][0]) == 1):
+        if int(R[i][2]) == A[len(A)-1]:
+
+            print(tree.sumRangep(int(R[i][1]),int(R[i][2])-1))
+        #print(tree.st.par)
         else:
-            print(tree.sumRange(R[i][1], R[i][2],1))
-    elif(R[i][0] == 2):
-         print("impa")
-        # if R[i][2] == A[len(A) - 1]:
-        #      print(tree.sumRange(R[i][1], R[i][2] - 1,2))
-        # else:
-        #     print(tree.sumRange(R[i][1], R[i][2], 2))
+            print(tree.sumRangep(int(R[i][1]), int(R[i][2])))
+    elif(int(R[i][0]) == 2):
+        if int(R[i][2]) == A[len(A) - 1]:
+            print(tree.sumRangei(int(R[i][1]), int(R[i][2]) - 1))
+        else:
+            print(tree.sumRangei(int(R[i][1]), int(R[i][2])))
 
-    elif(R[i][0] == 0):
-        tree.update(R[i][1], R[i][2])
-
-
-
+    elif(int(R[i][0]) == 0):
+       # print(R[i][1],A[(R[i][2])])
+        tree.update(int(R[i][1]), int(R[i][2]))
+        #print(A)
